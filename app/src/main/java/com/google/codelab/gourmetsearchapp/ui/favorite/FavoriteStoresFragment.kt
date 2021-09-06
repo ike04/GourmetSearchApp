38c3256745c15d4ae4a4406cc0872752c67a5fca
ike04 <boc.0409@outlook.jp>
2021-09-06 14:20:48 +0900

    Apply Dagger

--- app/src/main/java/com/google/codelab/gourmetsearchapp/ui/favorite/FavoriteStoresFragment.kt
+++ app/src/main/java/com/google/codelab/gourmetsearchapp/ui/favorite/FavoriteStoresFragment.kt
@@ -6,14 +6,16 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.google.codelab.gourmetsearchapp.databinding.FragmentFavoriteStoresBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class FavoriteStoresFragment : Fragment() {
 
-    private lateinit var favoriteStoresViewModel: FavoriteStoresViewModel
+    private val viewModel: FavoriteStoresViewModel by viewModels()
     private lateinit var binding: FragmentFavoriteStoresBinding
 
     override fun onCreateView(
@@ -21,13 +23,10 @@ class FavoriteStoresFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        favoriteStoresViewModel =
-            ViewModelProvider(this).get(FavoriteStoresViewModel::class.java)
-
         binding = FragmentFavoriteStoresBinding.inflate(inflater, container, false)
 
         val textView: TextView = binding.textDashboard
-        favoriteStoresViewModel.text.observe(viewLifecycleOwner, Observer {
+        viewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return binding.root
