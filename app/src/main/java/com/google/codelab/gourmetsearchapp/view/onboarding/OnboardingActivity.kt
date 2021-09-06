f0189c2926688640b51bf2a3049951933e19cc2a
ike04 <boc.0409@outlook.jp>
2021-09-06 17:44:43 +0900

    Add OnboardingActivity

---
+++ app/src/main/java/com/google/codelab/gourmetsearchapp/view/onboarding/OnboardingActivity.kt
@@ -0,0 +1,12 @@
+package com.google.codelab.gourmetsearchapp.view.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.codelab.gourmetsearchapp.R
+
+class OnboardingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding)
+    }
+}
---
+++ app/src/main/java/com/google/codelab/gourmetsearchapp/view/onboarding/onboardingFragment.kt
@@ -0,0 +1,18 @@
+package com.google.codelab.gourmetsearchapp.view.onboarding
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.codelab.gourmetsearchapp.R
+
+class onboardingFragment : Fragment() {
+    
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_onboarding, container, false)
+    }
+}
