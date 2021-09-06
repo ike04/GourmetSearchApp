c09b7f62115c26342f28a96d2543e954903bc89b
ike04 <boc.0409@outlook.jp>
2021-09-06 13:32:32 +0900

    Fix file name and resources

--- app/src/main/java/com/google/codelab/gourmetsearchapp/ui/favorite/FavoriteStoresFragment.kt
+++ app/src/main/java/com/google/codelab/gourmetsearchapp/ui/favorite/FavoriteStoresFragment.kt
@@ -1,4 +1,4 @@
-package com.google.codelab.gourmetsearchapp.ui.dashboard
+package com.google.codelab.gourmetsearchapp.ui.favorite
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -8,13 +8,13 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.google.codelab.gourmetsearchapp.R
-import com.google.codelab.gourmetsearchapp.databinding.FragmentDashboardBinding
+import com.google.codelab.gourmetsearchapp.databinding.FragmentFavoriteStoresBinding
 
-class DashboardFragment : Fragment() {
 
-    private lateinit var dashboardViewModel: DashboardViewModel
-    private var _binding: FragmentDashboardBinding? = null
+class FavoriteStoresFragment : Fragment() {
+
+    private lateinit var favoriteStoresViewModel: FavoriteStoresViewModel
+    private var _binding: FragmentFavoriteStoresBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,14 +25,14 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+        favoriteStoresViewModel =
+            ViewModelProvider(this).get(FavoriteStoresViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentFavoriteStoresBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
+        favoriteStoresViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
--- app/src/main/java/com/google/codelab/gourmetsearchapp/ui/map/MapViewModel.kt
+++ app/src/main/java/com/google/codelab/gourmetsearchapp/ui/map/MapViewModel.kt
@@ -1,10 +1,10 @@
-package com.google.codelab.gourmetsearchapp.ui.notifications
+package com.google.codelab.gourmetsearchapp.ui.map
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class MapViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is notifications Fragment"
