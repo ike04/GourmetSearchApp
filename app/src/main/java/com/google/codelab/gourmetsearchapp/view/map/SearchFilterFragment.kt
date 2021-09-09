a1ab65621882b36ce1ae2da1ea2a0447a2747980
ike04 <boc.0409@outlook.jp>
2021-09-09 09:27:24 +0900

    Add SearchFilterFragment

--- app/src/main/java/com/google/codelab/gourmetsearchapp/view/map/SearchFilterFragment.kt
+++ app/src/main/java/com/google/codelab/gourmetsearchapp/view/map/SearchFilterFragment.kt
@@ -6,12 +6,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioButton
+import androidx.fragment.app.viewModels
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.google.codelab.gourmetsearchapp.R
 import com.google.codelab.gourmetsearchapp.databinding.FragmentSearchFilterBinding
+import com.google.codelab.gourmetsearchapp.viewmodel.MapsViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SearchFilterFragment : BottomSheetDialogFragment() {
     private lateinit var binding: FragmentSearchFilterBinding
+    private val parentViewModel: MapsViewModel by viewModels()
 
     companion object {
         fun newInstance(): SearchFilterFragment {
