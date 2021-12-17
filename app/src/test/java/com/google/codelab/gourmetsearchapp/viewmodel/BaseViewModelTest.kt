9b11d49ae0758fda37d22ab5855fba28b12e07b7
ike04 <boc.0409@outlook.jp>
2021-12-17 18:23:29 +0900

    Refactoring codes

--- app/src/test/java/com/google/codelab/gourmetsearchapp/viewmodel/BaseViewModelTest.kt
+++ app/src/test/java/com/google/codelab/gourmetsearchapp/viewmodel/BaseViewModelTest.kt
@@ -1,16 +1,11 @@
 package com.google.codelab.gourmetsearchapp.viewmodel
 
-import com.google.codelab.gourmetsearchapp.R
 import com.google.codelab.gourmetsearchapp.model.Failure
 import com.google.codelab.gourmetsearchapp.usecase.BaseUsecase
 import com.google.codelab.gourmetsearchapp.usecase.Usecase
-import io.reactivex.rxjava3.core.Observable
-import io.reactivex.rxjava3.kotlin.addTo
-import io.reactivex.rxjava3.kotlin.subscribeBy
 import io.reactivex.rxjava3.schedulers.Schedulers
 import io.reactivex.rxjava3.subjects.PublishSubject
 import org.junit.Before
-import org.junit.Test
 import org.mockito.Mock
 import org.mockito.MockitoAnnotations
 import org.mockito.kotlin.given
