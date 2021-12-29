9a57023fae11f57ee873f7331824c70b8cfa6f4f
ike04 <boc.0409@outlook.jp>
2021-12-29 11:52:21 +0900

    Fix error logic and loading logic

--- app/src/main/java/com/google/codelab/gourmetsearchapp/usecase/Usecase.kt
+++ app/src/main/java/com/google/codelab/gourmetsearchapp/usecase/Usecase.kt
@@ -1,9 +1,9 @@
 package com.google.codelab.gourmetsearchapp.usecase
 
-import com.google.codelab.gourmetsearchapp.model.Failure
 import io.reactivex.rxjava3.core.Observable
 
 interface Usecase {
     fun dispose()
-    fun errorSignal(): Observable<Failure>
+    fun loadingSignal(): Observable<Boolean>
+    fun errorSignal(): Observable<Pair<Throwable, () -> Unit>>
 }
